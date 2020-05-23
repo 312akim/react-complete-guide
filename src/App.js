09eb213f2c888b967bbf4c81504245d0522f5d57
Alex Kim <312akim@gmail.com>
2020-05-23 03:45:10 -0500

    Dynamic & Flexible content

--- src/App.js
+++ src/App.js
@@ -5,21 +5,37 @@ import Person from './Person/Person';
 class App extends Component { // only component inherited components can have state
   state = {  //vWhen state is changed, React re-renders DOM
     persons: [
-      { name: "Max", age: 28 },
-      { name: "Manu", age: 29 },
-      { name: "Stephanie", age: 26 }
-    ]
+      { id: "uniqueId1", name: "Max", age: 28 },
+      { id: "uniqueId2", name: "Manu", age: 29 },
+      { id: "uniqueId3", name: "Stephanie", age: 26 }
+    ],
+    otherState: "some other value",
+    showPersons: false
   }
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(person => {
+      return person.id === id; //Return true or false
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons})
+  }
+
 // Handler is used to indicate a method not actively being called but assigning as an event handler.
-  switchNameHandler = (newName) => {
-    this.setState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: "Manu", age: 29 },
-        { name: "Stephanie", age: 29 }
-      ],
-      showPersons: false
-    })
+  deletePersonHandler = (personIndex) => {
+    // const persons = this.state.persons.slice //Slice with 0 args simply copies array
+    const persons = [...this.state.persons]; //Copy array with spread to ensure object remains immutable
+    persons.splice(personIndex, 1); //Removes 1 element from array starting at personIndex
+    this.setState({persons: persons})
   }
 
   togglePersonsHandler = () => {
@@ -37,22 +53,21 @@ class App extends Component { // only component inherited components can have st
     };
     
     // Dynamic Content 
-    let persons = null;
+    let persons = null; //Default no list of people until populated below.
 
     if (this.state.showPersons) {
-      persons = (
-        <div>
-          <Person 
-            name={this.state.persons[0].name} 
-            age={this.state.persons[0].age}/>
-          <Person 
-            name={this.state.persons[1].name} 
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, "Max!") /* Passing method as props. .bind to pass information more efficient than arrow function */}
-            >My Hobbies: Racing</Person>
-          <Person 
-            name={this.state.persons[2].name} 
-            age={this.state.persons[2].age}/>
+      persons = ( //Dynamic Listing of X elements
+        <div>          {/* Map accepts 2 arg. 1, each element name, 2 index */}
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person 
+                key={person.id /* Do not use index as it changes frequently w/ updated data */}
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                changed={(event) => this.nameChangedHandler(event, person.id)}
+              />
+          )})}
         </div>
       )
     }
